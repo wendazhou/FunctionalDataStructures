67e2ccee36d11deb7cdd35324e301a5361a487e1
Wenda Zhou <zhouwenda@outlook.com>
2013-10-04 10:39:55 +0100

    add benchmark for red black tree deletion

--- Benchmarks/redblack_tree_benchmarks.cpp
+++ Benchmarks/redblack_tree_benchmarks.cpp
@@ -64,3 +64,78 @@ BENCHMARK_F(RedBlackTreeInsert, FDS_RedBlackTree_Insert_Keep_All, RedBlackTreeIn
 		trees[i] = set;
 	}
 }
+
+class RedBlackTreeFindDeleteFixture
+	: public celero::TestFixture
+{
+public:
+	RedBlackTreeFindDeleteFixture()
+		: removed(element_count)
+	{
+		std::mt19937  mt(123);
+		std::uniform_int_distribution<int> dis;
+
+		for (std::size_t i = 0; i < element_count; i++)
+		{
+			int value = dis(mt);
+			data.push_back(value);
+			std::tie(fds_tree, std::ignore, std::ignore) = fds_tree.insert(value);
+			std_set.insert(value);
+		}
+	}
+
+	std::vector<int> data;
+	fds::redblack_tree<int> fds_tree;
+	std::set<int> std_set;
+
+	std::vector<fds::redblack_tree<int>> removed;
+
+	static const std::size_t element_count = 1000;
+};
+
+BASELINE_F(RedBlackTreeFind, STD_Set_Find, RedBlackTreeFindDeleteFixture, 0, 1000)
+{
+	for (std::size_t i = 0; i < element_count; i++)
+	{
+		data[i] = *std_set.find(data[i]);
+	}
+}
+
+BENCHMARK_F(RedBlackTreeFind, FDS_RedBlackTree_Find, RedBlackTreeFindDeleteFixture, 0, 1000)
+{
+	for (std::size_t i = 0; i < element_count; i++)
+	{
+		data[i] = *fds_tree.find(data[i]);
+	}
+}
+
+BASELINE_F(RedBlackTreeDelete, STD_Set_Delete, RedBlackTreeFindDeleteFixture, 0, 10)
+{
+	std::set<int> set = std_set;
+
+	for (auto i : data)
+	{
+		set.erase(i);
+	}
+}
+
+BENCHMARK_F(RedBlackTreeDelete, FDS_RedBlackTree_Delete_Keep_One, RedBlackTreeFindDeleteFixture, 0, 10)
+{
+	fds::redblack_tree<int> tree = fds_tree;
+
+	for (auto i : data)
+	{
+		std::tie(tree, std::ignore) = tree.erase(i);
+	}
+}
+
+BENCHMARK_F(RedBlackTreeDelete, FDS_RedBlackTree_Delete_Keep_All, RedBlackTreeFindDeleteFixture, 0, 10)
+{
+	fds::redblack_tree<int> tree = fds_tree;
+
+	for (std::size_t i = 0; i < element_count; i++)
+	{
+		std::tie(tree, std::ignore) = tree.erase(i);
+		removed[i] = tree;
+	}
+}
